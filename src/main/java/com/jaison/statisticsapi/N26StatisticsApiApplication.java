b47778bf4ec22db3b763c47987c9e4b3d7b614c0
jado <jaison.dossantos@crealogix.com>
2020-10-19 00:21:42 +0200

    Initial commit - creating project

---
+++ src/main/java/com/jaison/statisticsapi/N26StatisticsApiApplication.java
@@ -0,0 +1,13 @@
+package com.jaison.statisticsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class N26StatisticsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(N26StatisticsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaison/statisticsapi/N26StatisticsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaison.statisticsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class N26StatisticsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
